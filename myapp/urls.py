e2bdb2c9ae5c46610f60d408d7ece64cfbbdb7e6
Raashidah <rashidapaloli18@gmail.com>
2021-05-18 20:31:07 +0530

    add

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 urlpatterns = [
     path('test',views.test1,name='test'),
-    path('app',views.test2,name='app'),
     path('index',views.test3,name='index'),
     path('head',views.test4,name='head'),
     path('clg',views.test5,name='clg'),
-    path('login',views.test6,name='login')
+    path('login',views.test6,name='login'),
+    path('slide',views.test7,name='slide'),
+    path('fb',views.test8,name='fb'),
+    
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def test1(request):
-    return HttpResponse("hello world")
-def test2(request):
-    return HttpResponse("<H1>HIIIIIIIII</H1>") 
+    return HttpResponse("hello world") 
 def test3(request):
     return render(request,'index.html')
 def test4(request):
-    return render(request,'head.html')    
+    return render(request,'head.html') 
 def test5(request):
-    return render(request,'college.html')  
+    return render(request,'college.html')       
+def test7(request):
+    return render(request,'anim.html') 
+def test8(request):
+    return render(request,'facebook.html')     
 def test6(request):
     return render(request,'login.html')      
